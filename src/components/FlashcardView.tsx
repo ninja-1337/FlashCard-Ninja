21a7d85e116bf34fe51bf1f97e04d19ace253807
unknown <you@example.com>
2023-06-02 20:02:20 +0300

    init

--- src/components/FlashcardView.tsx
+++ src/components/FlashcardView.tsx
@@ -28,7 +28,7 @@ export default function ChatLine( id :any ) {
   return (
 
 
-<div className="card sm:h-screen w-2/3 h-2/3 rounded-lg shadow-lg bg-gradient-to-r from-blue-500 to-purple-500" onClick={toggleState}>
+<div className="card sm:h-screen h-60vh w-2/3 h-2/3 rounded-lg shadow-lg bg-gradient-to-r from-blue-500 to-purple-500" onClick={toggleState}>
       {isFront ? <FrontContent /> : <BackContent />}
     </div>
 
@@ -44,7 +44,7 @@ function FrontContent() {
 }
 
 function BackContent() {
-  return    <div className="card h- p-16 w-full h-full rounded-lg shadow-lg bg-gradient-to-r from-red-500 to-purple-500">
+  return    <div className="card h-full p-16 w-full h-full rounded-lg shadow-lg bg-gradient-to-r from-red-500 to-purple-500">
       {/* Add the content of the card here */}
       <h1>Back</h1>
     </div>;
