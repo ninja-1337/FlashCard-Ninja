31ecca67ad9ed080e506238f938d6591cc98d938
unknown <you@example.com>
2023-06-07 01:42:25 +0300

    pwa init

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,11 @@
-// @ts-nocheck
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
- * This is especially useful for Docker builds.
- */
-!process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
-
-/** @type {import("next").NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
+});
+ 
+const nextConfig = withPWA({
   reactStrictMode: true,
-  swcMinify: true,
-  
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  images: {
-    domains: ["https:/cdn.discordapp.com", "cdn.discordapp.com","https://lh3.googleusercontent.com","lh3.googleusercontent.com"],
-  },
-  disable: process.env.NODE_ENV === 'development'
-})
-
-/** @type {import("next").NextConfig} */
-const config = {
-  reactStrictMode: true,
-  swcMinify: true,
-  
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  images: {
-    domains: ["https:/cdn.discordapp.com", "cdn.discordapp.com","https://lh3.googleusercontent.com","lh3.googleusercontent.com"],
-  },
-  
-};
-
-export default withPWA;
+});
+ 
+module.exports = nextConfig;
