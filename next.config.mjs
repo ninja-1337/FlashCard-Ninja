9132d29fdf80bb0b50b602dba111388482679e8b
unknown <you@example.com>
2023-06-07 01:26:14 +0300

    pwa init

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,12 @@
  * This is especially useful for Docker builds.
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development'
+})
 
 /** @type {import("next").NextConfig} */
 const config = {
@@ -20,4 +26,5 @@ const config = {
   
 };
 
-export default config;
+// @ts-ignore
+export default withPWA(config);
