14349e7963a41beb0661b7edc8b339f2a7826143
unknown <you@example.com>
2023-06-07 22:53:36 +0300

    pwa init

--- public/worker.js
+++ public/worker.js
@@ -5,3 +5,35 @@ self.addEventListener('fetch', e => {
      return <>Congratz</>;
     }
   })
+
+  function isPushSupported() {
+    //checks if user has granted permission to Push notifications
+    if (Notification.permission === 'denied') {
+      alert('User has blocked push notification.');
+      return;
+    }
+ 
+    //Checks if current browser supports Push notification
+    if (!('PushManager' in window)) {
+      alert('Sorry, Push notification isn\'t supported in your browser.');
+      return;
+    }
+    }
+  
+    
+function subscribePush() {
+    //Subscribes user to Push notifications
+    registration.pushManager.subscribe({
+      userVisibleOnly: true //Set user to see every notification
+    })
+    .then(function (subscription) {
+      toast('Subscribed successfully.');
+      console.info('Push notification subscribed.');
+      console.log(subscription);
+    })
+    .catch(function (error) {
+      console.error('Push notification subscription error: ', error);
+    });
+  }
+
+  subscribePush();
