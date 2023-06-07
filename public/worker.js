76c6914ce2b23d8c09a5952f5f932105ac7abdfa
unknown <you@example.com>
2023-06-07 15:23:17 +0300

    pwa init

--- public/worker.js
+++ public/worker.js
@@ -1,1 +1,7 @@
 console.log("hello from worker")
+
+self.addEventListener('fetch', e => {
+    if (e.request.method === 'POST' && e.request.url.endsWith('/share-target')) {
+     return <>Congratz</>;
+    }
+  })
