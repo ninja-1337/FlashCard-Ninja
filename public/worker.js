6a8d72dd463732b85169258123e7c1200712ec36
unknown <you@example.com>
2023-06-07 11:15:49 +0300

    pwa init

--- public/worker.js
+++ public/worker.js
@@ -2,7 +2,26 @@ const CACHE_VERSION = 1;
 const CURRENT_CACHES = {
   font: `font-cache-v${CACHE_VERSION}`,
 };
-
+self.addEventListener("fetch", (event) => {
+    // Regular requests not related to Web Share Target.
+    if (event.request.method !== "POST") {
+      event.respondWith(fetch(event.request));
+      return;
+    }
+  
+    // Requests related to Web Share Target.
+    event.respondWith(
+      (async () => {
+        const formData = await event.request.formData();
+        const link = formData.get("link") || "";
+        // Instead of the original URL `/save-bookmark/`, redirect
+        // the user to a URL returned by the `saveBookmark()`
+        // function, for example, `/`.
+        const responseUrl = await saveBookmark(link);
+        return Response.redirect(responseUrl, 303);
+      })()
+    );
+  });
 
 self.addEventListener("fetch", (event) => {
   console.log("Handling fetch event for", event.request.url);
