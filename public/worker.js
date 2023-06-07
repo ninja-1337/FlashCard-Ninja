5c67be967569e8f1b1eea26fff3a4584e709f724
unknown <you@example.com>
2023-06-07 13:30:25 +0300

    pwa init

--- public/worker.js
+++ public/worker.js
@@ -1,24 +1,23 @@
-self.addEventListener("fetch", (event) => {
-    console.log(`Handling fetch event for ${event.request.url}`);
+// self.addEventListener("fetch", (event) => {
+//     console.log(`Handling fetch event for ${event.request.url}`);
   
-    event.respondWith(
-      caches.match(event.request).then((response) => {
-        if (response) {
-          console.log("Found response in cache:", response);
-          return response;
-        }
-        console.log("No response found in cache. About to fetch from network…");
+//     event.respondWith(
+//       caches.match(event.request).then((response) => {
+//         if (response) {
+//           console.log("Found response in cache:", response);
+//           return response;
+//         }
+//         console.log("No response found in cache. About to fetch from network…");
   
-        return fetch(event.request)
-          .then((response) => {
-            console.log("Response from network is:", response);
-  
-            return response;
-          })
-          .catch((error) => {
-            console.error(`Fetching failed: ${error}`);
-            throw error;
-          });
-      })
-    );
-  });
+//         return fetch(event.request)
+//           .then((response) => {
+         
+//             return response;
+//           })
+//           .catch((error) => {
+//             console.error(`Fetching failed: ${error}`);
+//             throw error;
+//           });
+//       })
+//     );
+//   });
