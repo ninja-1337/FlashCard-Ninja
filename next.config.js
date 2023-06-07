1ef9e2f3a9a916b5eb927bcf90498f5c93c02391
unknown <you@example.com>
2023-06-07 03:17:16 +0300

    pwa init

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
   skipWaiting: true,
+  publicExcludes: [
+    '!robots.txt',
+    '!sitemap.xml.gz',
+],
   disable: process.env.NODE_ENV === 'development'
 })
 /** @type {import('next').NextConfig} */
