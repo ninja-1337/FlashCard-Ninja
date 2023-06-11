40d4d31695f17c2f43aaa0789c57fe3773cf3247
unknown <you@example.com>
2023-06-11 17:32:26 +0300

    share target

--- src/pages/api/file-collector.ts
+++ src/pages/api/file-collector.ts
@@ -1,4 +1,11 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '15mb',
+    },
+  },
+}
 
 const filecollector = async (req: NextApiRequest, res: NextApiResponse) => {
  
