7e2f98e61117ceb39f42faca23e698d9e0ec2faf
unknown <you@example.com>
2023-06-11 17:23:56 +0300

    share target

--- src/pages/file-collector.tsx
+++ src/pages/file-collector.tsx
@@ -12,7 +12,7 @@ const collector: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#0b3bd6] to-[#f8a221]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            About <span className="text-[hsl(280,100%,70%)]">FlashCard</span> Ninja
+            TSTR <span className="text-[hsl(280,100%,70%)]">FlashCard</span> Ninja
           </h1>
           <h1 className="text-5xl font-extrabold leading-normal text-white md:text-[5rem]">
             <span className="text-2xl font-bold text-white"></span>
