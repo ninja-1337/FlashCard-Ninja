fad8a3cfcc8c7b49323de862baa56d70e1748e68
unknown <you@example.com>
2023-06-12 21:11:17 +0300

    file share

--- src/app/api/file-collector/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
- 
-export async function GET(request: Request) {
-console.log("Here Test")
-  const res = "ggggg"
-  const product = res
- 
-  return NextResponse.json({ product })
-}
---
+++ src/pages/api/file-collector.ts
@@ -0,0 +1,33 @@
+
+import formidable from 'formidable';
+import { NextRequest, NextResponse } from "next/server";
+
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export const config = {
+
+   api: {
+    bodyParser: false
+
+  },
+}
+
+const filecollector = async (req : NextRequest, res : NextApiResponse) => {
+  const data = await req;
+  let resp;
+  if (req.method === "PUT") {
+  
+  }
+  if (req.method === "GET") {
+
+  }
+  if (req.method === "POST") {
+  console.log(req)
+
+   resp="Post: "+data.body
+  }
+
+  res.status(200).json(data);
+};
+
+export default filecollector;
