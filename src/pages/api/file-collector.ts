462c684de33726185ae8e3a8b59368ad43d45de7
unknown <you@example.com>
2023-06-12 19:34:43 +0300

    file share

--- src/pages/api/file-collector.ts
+++ src/pages/api/file-collector.ts
@@ -1,5 +1,6 @@
 
-import { Form } from '@ts-stack/multiparty';
+import formidable from 'formidable';
+
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export const config = {
@@ -11,30 +12,36 @@ export const config = {
 }
 
 const filecollector = async (req : NextApiRequest, res : NextApiResponse) => {
-//   let data = req.body;
-//   let names;
-//   let url;
-//   let resp;
-//   if (req.method === "PUT") {
-//     data="Put: "+data
-//   }
-//   if (req.method === "GET") {
-//     data="GET: "+data
-//   }
-//   if (req.method === "POST") {
-//   console.log(req.body)
-//    names=data.title
-//    url=data.url
-//    resp="Post: "+data
-//   }
-// resp=  JSON.parse(req.body)
-const form = new Form();
+  // let data = req.body;
+  // let names;
+  // let url;
+  // let resp;
+  // if (req.method === "PUT") {
+  //   data="Put: "+data
+  // }
+  // if (req.method === "GET") {
+  //   data="GET: "+data
+  // }
+  // if (req.method === "POST") {
+  // console.log(req.body)
+  //  names=data.title
+  //  url=data.url
+  //  resp="Post: "+data
+  // }
+  
 
-const x= form.parse(req, (err, fields, files) => {
+  const form = formidable({ multiples: true });
 
-});
-
-  res.status(200).json(x);
+  let inputFields = {};
+  
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+    
+      return;
+    }
+    inputFields = fields;
+  });
+  res.status(200).json(inputFields);
 };
 
 export default filecollector;
