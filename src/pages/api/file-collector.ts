8662f1fe8c115769d1ab3b20d7ca5004ec2c3736
unknown <you@example.com>
2023-06-12 20:00:10 +0300

    file share

--- src/pages/api/file-collector.ts
+++ src/pages/api/file-collector.ts
@@ -12,39 +12,24 @@ export const config = {
 }
 
 const filecollector = async (req : NextApiRequest, res : NextApiResponse) => {
-  // let data = req.body;
-  // let names;
-  // let url;
-  // let resp;
-  // if (req.method === "PUT") {
-  //   data="Put: "+data
-  // }
-  // if (req.method === "GET") {
-  //   data="GET: "+data
-  // }
-  // if (req.method === "POST") {
-  // console.log(req.body)
-  //  names=data.title
-  //  url=data.url
-  //  resp="Post: "+data
-  // }
+  let data = req.body;
+  let names;
+  let url;
+  let resp;
+  if (req.method === "PUT") {
+    data="Put: "+data
+  }
+  if (req.method === "GET") {
+    data="GET: "+data
+  }
+  if (req.method === "POST") {
+  console.log(req.body)
+   names=data.title
+   url=data.url
+   resp="Post: "+data
+  }
   
-
-  const form = formidable({ multiples: true });
-
-  let inputFields = {};
-  
-  form.parse(req.body, (err, fields, files) => {
-    if (err) {
-    
-      return;
-    }
-    console.log(files)
-    console.log(fields)
-    inputFields = fields;
-    res.status(200).json({ fields, files })
-  });
-  res.status(200).json(inputFields);
+  res.status(200).json(req.body);
 };
 
 export default filecollector;
